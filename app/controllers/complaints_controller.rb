6d728452e12dba693fc1c99f902b1137b67acf0c
Fernando Uval <fernandouval@gmail.com>
2023-04-18 03:09:15 -0300

    Primer versión de maquetado funcional

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,2 +1,31 @@
 class ComplaintsController < ApplicationController
+  def index
+    respond_to do |format|
+      format.js {
+        if params[:step].present?
+          @step = params[:step].to_i + 1
+          #Add logic to each step
+          case params[:step]
+          when 2
+            #process initial data and respond accorded
+          when 5
+            #skip 6
+            @step += 1
+            #SAVE DATA
+          when 7
+            #skip 8 - END
+          else
+            @complaint = Complaint.new
+          end
+        else
+          @step = 1
+          @complaint = Complaint.new
+        end
+      }
+      format.html{
+        @step = 1
+        @complaint = Complaint.new
+      }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "static_pages#home"
+  get '/sobre-el-proyecto' => 'static_pages#about'
+  get '/nueva-apelacion' => 'complaints#index'
+  get '/preguntas-frecuentes' => 'static_pages#faqs'
+
+  resources :complaints
 end
