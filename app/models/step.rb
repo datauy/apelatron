967aae39b11600114b88724e3964d96badeef789
Fernando Uval <fernandouval@gmail.com>
2023-06-30 02:35:34 -0300

    Se corrige bug en pasos, footer sobre el proyecto, delete de steps y loading para paso 2 a 3

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,7 +1,7 @@
 class Step < ApplicationRecord
   belongs_to :platform
   belongs_to :reason
-  has_many :step_relations
+  has_many :step_relations, dependent: :destroy
   has_many :standards, through: :step_relations
   
   validates :step_number, :presence => true
